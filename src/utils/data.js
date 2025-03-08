4a1f6be1be3fe2067830199fadffb52c563d3a84
@muqarram.asad <mukarram.asad@hysabkytab.com>
2025-03-08 23:37:18 +0500

    Added animation on scrolling

--- src/utils/data.js
+++ src/utils/data.js
@@ -14,10 +14,10 @@ export const SKILLS = [
     title: "Backend",
     icon: "frontend.png",
     skills: [
-      { skill: "Node.js", percentage: "70%" },
-      { skill: "Express.js", percentage: "65%" },
+      { skill: "Node.js & Express.js", percentage: "80%" },
+      { skill: "Java", percentage: "70%" },
+      { skill: "MongoDB", percentage: "75%" },
       { skill: "Firebase Database", percentage: "70%" },
-      { skill: "Git & Github", percentage: "85%" },
     ],
   },
 
@@ -25,9 +25,9 @@ export const SKILLS = [
     title: "Tools",
     icon: "backend.png",
     skills: [
-      { skill: "Responsive Design", percentage: "95%" },
-      { skill: "VS Code", percentage: "90%" },
-      { skill: "Postman", percentage: "75%" },
+      { skill: "VS Code || Sublime Merge", percentage: "95%" },
+      { skill: "Git & (Github || GitLab)", percentage: "90%" },
+      { skill: "Postman", percentage: "85%" },
       { skill: "Figma", percentage: "80%" },
     ],
   },
@@ -36,51 +36,49 @@ export const SKILLS = [
     title: "Soft Skills",
     icon: "soft_skills.png",
     skills: [
-      { skill: "Problem Solving", percentage: "80%" },
+      { skill: "Problem Solving", percentage: "90%" },
       { skill: "Attention to Details", percentage: "85%" },
-      { skill: "Collaboration", percentage: "80%" },
-      { skill: "Ethics & Attitude", percentage: "88%" },
+      { skill: "Collaboration", percentage: "95%" },
+      { skill: "Ethics & Attitude", percentage: "90%" },
     ],
   },
 ];
 
 export const WorkExperienceData = [
   {
-    title: "Junior Full Stack Developer",
-    company: "Hysab Kytab (Pakistan)",
-    duration: "March 2023 - Present",
+    title: "Full Stack Developer (Frontend Focused)",
+    company: "Hysab Kytab Private Limited (Jaffer Business Systems)",
+    duration: "March 2024 - Present",
     description: [
-      "According to the position, I have to understand and work with both frontend & backend teams of the app. ",
-      "But due to junior level, I am working with frontend team mostly.",
-      "But side by side, I am learning backend technologies. The technologies are",
-      "Node.js, Express.js, Nest.js, MySql Database",
-
+      "With respect to this role, I have to work with both frontend & backend teams of the app. ",
+      "But due to more experience in frontend development, I usually work with frontend team.",
+      "But side by side, I am learning backend technologies. The technologies are:",
+      "Node.js, Express.js, Nest.js, MySql & MongoDB databases.",
     ],
   },
   {
     title: "Frontend Developer MTO",
-    company: "Hysab Kytab (Pakistan)",
+    company: "Hysab Kytab Private Limited (Jaffer Business Systems)",
     duration: "Aug 2023 - Feb 2024",
     description: [
-      "The role of Management Trainee Officer(MTO) is different from Internship.",
-      "Here I have to know each and every thing of our product.",
-      "Like how the project is working and how I can contribute to it.",
-      "Make accurate and attractive designs w.r.t Figma Prototype.",
+      "The role of Management Trainee Officer(MTO) is little bit different from Internship.",
+      "Here in this role, now I have to work on the product revamp with the frontend team.",
+      "First I have to make accurate and attractive designs w.r.t Figma Prototype and then develop it.",
     ],
   },
 
   {
     title: "Frontend Developer Intern",
-    company: "Hysab Kytab (Pakistan)",
+    company: "Hysab Kytab Private Limited (Jaffer Business Systems)",
     duration: "Feb 2023 - July 2023",
     description: [
-      "In this internship, I worked closely with the frontend team to  built the UI of the Hysab Kytab App.",
-      "My core responsibility was to understand the flow of the app and then develop it.",
-      "I learned alot of technologies related to the frontend development.",
-      "React.js, Figma, Modular CSS, and responsive design.",
+      "During this internship, I collaborated closely with the frontend team to develop the UI of the Hysab Kytab App.",
+      "My primary responsibilities included focused on learning and applying frontend development best practices and understanding the app's workflow and implementing it effectively.",
+      "I gained hands-on experience with various frontend technologies and tools.",
+      "Technologies and tools that I learned and used: React.js, Figma, Modular CSS, responsive design and PWA.",
     ],
   },
- 
+
   {
     title: "Web Developer Intern",
     company: "The Sparks Foundation (Malaysia)",
@@ -92,7 +90,4 @@ export const WorkExperienceData = [
       "HTML5, CSS3, JavaScript & EcmaScript6, and Firebase.",
     ],
   },
-
- 
- 
 ];
